bc605a80bd9601620f1b255ac4816ad090ff338c
Logan Rose <logprose@umich.edu>
2025-12-03 21:31:33 -0500

    feat: working day 1

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,39 +2,44 @@
 #include <iostream>
 #include <string>
 
-
-
 int main() {
-    std::vector<std::vector<int>> input_vecs;
-    std::ifstream inputFile("./input.txt");
-    if (!inputFile.is_open()) {
-        std::cerr << "Error opening file!" << std::endl;
-        return 1;
+  std::vector< std::vector<int> > input_vecs;
+  std::ifstream inputFile("./input.txt");
+  if (!inputFile.is_open()) {
+    std::cerr << "Error opening file!" << std::endl;
+    return 1;
+  }
+  std::string line;
+  while (std::getline(inputFile, line)) {
+    std::vector<int> temp;
+    for (auto &i : line) {
+      temp.push_back(i - '0');
     }
-    std::string line;
-    while (std::getline(inputFile, line)) {
-        std::vector<int> temp;
-        for (auto &i : line) {
-            temp.push_back((int)i);
-        }
-        input_vecs.push_back(temp);
+    input_vecs.push_back(temp);
+  }
+  inputFile.close();
+  long long output = 0;
+  int highest = 0;
+  int second_highest = 0;
+  size_t highest_idx = 0;
+
+  for (auto &vec : input_vecs) {
+    highest_idx = 0;
+    highest = 0;
+    second_highest = 0;
+
+    for (size_t i = 0; i < vec.size() - 1; i++) {
+      int t_highest = highest;
+      highest = std::max(highest, vec[i]);
+      if (highest != t_highest) {
+        highest_idx = i + 1;
+      }
     }
-    inputFile.close();
-    long long output = 0;
-    int highest = 0;
-    int second_highest = 0;
-    for (auto &vec : input_vecs) {
-        for (size_t i = 0; i < vec.size(); i++) {
-            int t_highest = highest;
-            highest = std::max(highest, vec[i]);
-            if (highest != t_highest) {
-                second_highest = 0;
-            } else {
-                second_highest = std::max(second_highest, vec[i]);
-            }
-        }
-        int num = highest * 10 + second_highest;
-        output += num;
+    for (size_t j = highest_idx; j < vec.size(); j++) {
+      second_highest = std::max(second_highest, vec[j]);
     }
-    std::cout << output << std::endl;
+    int num = highest * 10 + second_highest;
+    output += num;
+  }
+  std::cout << output << std::endl;
 }
