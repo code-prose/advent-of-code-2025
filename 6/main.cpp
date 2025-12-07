3a61f82b1fe27ebaac2c83c601e51665d325bfaf
Logan Rose <logprose@umich.edu>
2025-12-06 21:52:25 -0500

    feat: day 6 input reading

--- 6/main.cpp
+++ 6/main.cpp
@@ -2,30 +2,46 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <sstream>
+#include <vector>
 
 
-using ReadVec = std::vector<std::string>;
+using ReadVec = std::vector< std::vector<int> >;
 void pt1();
 void pt2();
-void parseInput();
+std::vector<std::string> splitStringStream(const std::string& s, char delimiter);
 
 
 int main() {
   ReadVec InputVector;
-  std::ifstream inputFile("./input.txt");
+  std::ifstream inputFile("./test_input.txt");
   if (!inputFile.is_open()) {
     std::cerr << "Error opening file!" << std::endl;
     return 1;
   }
   std::string line;
+  int i = 0;
   while (std::getline(inputFile, line)) {
-    std::string temp;
-    for (auto &i : line) {
-      temp.push_back(i);
-    }
-    InputVector.push_back(temp);
+        std::vector<int> temp;
+        std::vector<std::string> results = splitStringStream(line, ' ');
+        for (auto &res: results) {
+            temp.push_back(stoi(res));
+        }
+        InputVector.push_back(temp);
   }
+  // std::cout << InputVector << std::endl;
+  // pt1();
+  // pt2();
 
-  pt1();
-  pt2();
 }
+
+std::vector<std::string> splitStringStream(const std::string& s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::stringstream ss(s);
+    while (std::getline(ss, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
